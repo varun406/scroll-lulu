209e068480a8f841f31e835d1b7a55fd014148a9
varun406 <varunnayak46@gmail.com>
2025-01-07 17:13:01 +0530

    refactor: removed unused index.css file and changed height of main section top wrapper to 101vh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div style={{ height: "100vh" }}></div>
+      <div style={{ height: "101vh" }}></div>
       <PhoneSection />
       <div style={{ height: "100vh" }}></div>
     </div>
